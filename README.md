b1d75f9f3fc9043d80e94529082a3f0a0e6b50c8
Tankpt <Tankpt@126.com>
2015-03-28 00:00:54 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# slide
+##使用
+
+###1. 起步
+插件依赖于一个animate.css还有scroll.js两个文件，需要在页面中引入，其中scroll.js依赖于Zepto或者jQuery的几个方法，所以还需要引入zepto或者jQuery
+
+    <link type="text/css" href="animate.css" /> 
+    <script src="js/zepto.js"></script>
+    <script src="js/scroll.js"></script>
+
+###2. 基本模板
+
+    <div class="pt-pageList">
+		<div class="pt-page">/*直接加载出来*/</div>
+		<div class="pt-page" data-animate="bounceUp">
+		    /*以bounceUp方式动画*/
+		</div>
+		<div class="pt-page lazyload" data-animate="bounceUp" data-bg="">
+		    /*对有背景图的进行惰性加载，并以bounceUp方式动画*/
+		</div>
+		<div class="pt-page">
+		    <h1>/*页面切换完就有*/</h1>
+			<h1 class="animated bounceUp removeable delay2">/*切换完后延迟动画*/</h1>
+			<img data-src="" class="lazyload">/*每一页中的图片元素也支持惰性加载*/
+		</div>
+		...
+	</div>
+
+(1)　父元素上**需要**加类.pt-pageList(可以在less中修改，重新构建)
+(2)　子元素即每一页**需要**加类pt-page(可以在less中修改，重新构建)
+(3)　data-animate=""这里可以填写需要的页面切换之间的动画，一共有××种，详见文档最后的附录部分(关于这个animate属性，支持重新配置，如果有冲突可以在js中进行赋值)
+(4)　data-bg=""对于每一页设置背景图片还需要进行惰性加载的页面，可以将背景图的地址放在这里（暂不支持bg名字的重新配置），**同时元素需要再加一个类lazyload**
+(5)　对于每一个页面中的元素，可以自行进行定位样式的设置，如果需要对每一页中的元素进行动画配置，则只需要对该元素加上class为"animated removeable　bounceUp delay2"，除了延迟的类以外其他都是必须的且都不可配置
+        
+        bounceUp /*动画的名字，种类见附录*/
+        delay2 /*动画延迟时间，可自行定义类加入，若没有则无延迟*/
+        关于延迟，默认提供了四种方案(不够可自行进行写css)
+        A. delay1 :延迟0.2s
+        B. delay2 :延迟1S
+        C. delay3 :延迟1.2S
+        D. delay4 :延迟2S
+(6)　对于每一页的图片元素也支持惰性加载，只需要在img标签中加一个data-src（不可配置）和一个类lazyload（可配置），当然这里同时还可以继续添加动画
+
+
+###３.　使用方法
+
+    var slid = new SlidePage({
+        /*@required:传入父节点的类或者ＩＤ*/
+		container : "",
+		/*自定义节点动画属性data-animate，修改animate名字默认为animate*/
+		attr : "",
+		/*节点中的类lazyload，可以进行配置，不传试用默认*/
+		lazyClass : "",
+		/*自定义惰性加载的数量，默认是３，既页面中至少会加载三个页*/
+		preLoadNum : ,
+	}),
+    mainBody = $("#pt-main");
+    mainBody.on('swipeDown',function(){
+		slid.prePage();/*上一页*/
+	})
+
+	mainBody.on('swipeUp',function(){
+		slid.nextPage();/*下一页*/
+	})
+
+###4.　动画介绍
+
+动画来源自[Animate.css](https://github.com/daneden/animate.css)，这里仅仅是对动画进行整理分类，构建为less文件，原有的一些零散的动画，将他整理到一起，比如一个bounceUp动画，按照原来的路径退出，这里整理为bounceUp out的类，具体的动画支持如下
+(1)　bounce类
+
+    bounce、bounceUp、bounceRight、bounceDown、bounceLeft
+(2)　fade类
+
+    fade、fadeUp、fadeUpBig、fadeRight、fadeRightBig、fadeDown、fadeDownBig、fadeLeft、fadeLeftBig
+(3)　flip类
+
+    flipX、flipY
+(4)　rotate类
+
+    rotate、rotateDownLeft、rotateDownRight、rotateUpLeft、rotateUpRight
+(5) slide类
+
+    slideUp、slideRight、slideDown、slideLeft
+(6) zoom类
+
+    zoom、zoomUp、zoomRight、zoomDown、zoomLeft
--- js/scroll.js
+++ js/scroll.js
@@ -1,114 +1,163 @@
 /*
-* options :
+* @param options {
+*          container :   
+*        }
 *	1. container : 
-*	2. index :  0-..
+*	2. attr :  动画的data-**的属性名，防止冲突
 *	3. 
 */
 ;(function(window,$){
-
-  function whichTransitionEvent(){
-    var t,
-        el = document.createElement('fakeelement'),
-        transitions = {
-          'WebkitAnimation' : 'webkitAnimationEnd',
-          'OAnimation' : 'oAnimationEnd',
-          'msAnimation' : 'MSAnimationEnd',
-          'animation' : 'animationend'
-        };
-
-    for(t in transitions){
-      if( el.style[t] !== undefined ){
-        return transitions[t];
+    /*浏览器支持的动画事件*/
+  	var transitionEvent = (function(){
+      var t,
+          el = document.createElement('fakeelement'),
+          transitions = {
+            'WebkitAnimation' : 'webkitAnimationEnd',
+            'OAnimation' : 'oAnimationEnd',
+            'msAnimation' : 'MSAnimationEnd',
+            'animation' : 'animationend'
+          };
+      for(t in transitions){
+        if( el.style[t] !== undefined ){
+          return transitions[t];
+        }
       }
-    }
-  }
+    })();
 
-  var transitionEvent = whichTransitionEvent();
+  	var SlidePage = function(options){
+    	this.$container = $(options.container);
+    	this.attr = options.attr || 'animate';
+      this.lazyClass = options.delay || 'lazyload';
+      this.preLoadNum = options.preLoadNum || 3;
+    	this._initial();
+	  }
+    /*
+    *初始化：
+    */
+  	SlidePage.prototype._initial = function() {
+      $("html").height($(window).height());
+      $("html").width($(window).width());
+      this.index = 0 ;
+    	this.pageArray = this.$container.find(".pt-page");
+    	this.pageLength = this.pageArray.length;
+    	this.isAnimating  = false;
+      /*存放所有页面节点的动画队列*/
+    	this.animationArray = [];
+      /*存放所有页面中开启了延迟加载的动画队列*/
+      this.childrenNodes = [];
+      /*存放页面之中延迟加载的背景图*/
+      this.lazyArray =[];
 
-  var SlidePage = function(options){
-  		
-    this.$container = $(options.container);
-    this.index = options.index || 0 ;
-    this.attr = options.attr || 'animate';
-    this.initial();
-	}
+    	var self = this,
+        	attr = this.attr;
 
-  SlidePage.prototype.initial = function() {
-    this.pageArray = this.$container.find(".pt-page");
-    this.pageLength = this.pageArray.length;
-    this.isAnimating  = false;
-    this.animationArray = [];
+    	$.each(this.pageArray,function(index,item){
+    		var $item = $(item);
+    		self.animationArray.push($item.data(attr));
+        self.childrenNodes.push($item.children(".removeable"));
+        self.lazyArray.push(0);
+    		$item.css("z-index",index);
+    	});
 
-    var self = this,
-        attr = this.attr;
-    $.each(this.pageArray,function(index,item){
-      var $item = $(item);
-      self.animationArray.push($item.data(attr));
-      $item.css("z-index",index);
-    });
+      /**首页的动画*/
+      this.pageArray.eq(this.index).addClass("pt-page-current");
+      this._showPage(this.index);
+    };
 
-  };
-
-  SlidePage.prototype.animcursorCheck = function(param){
-    if(this.isAnimating){
-      return false;
-    }else{
-      return true;
-    }
-  };
-
-  SlidePage.prototype.nextPage = function() {
-    if(this.animcursorCheck('next') && this.index != this.pageLength -1){
+  	SlidePage.prototype.nextPage = function() {
+    	if(!this.isAnimating && this.index != this.pageLength -1){
       
-      var outClass = this.animationArray[this.index],
-          $outNode = this.pageArray.eq(this.index),
-          inClass = this.animationArray[++this.index],
-          $inNode = this.pageArray.eq(this.index),
-          self = this;
+    		var outIndex = this.index,
+            inIndex = ++this.index,
+            outClass = this.animationArray[outIndex],
+        		$outNode = this.pageArray.eq(outIndex),
+        		inClass = this.animationArray[inIndex]+' animated',
+        		$inNode = this.pageArray.eq(inIndex),
+        		self = this;
 
-      this.isAnimating = true;
-      
-      $inNode.addClass(inClass+' pt-page-current animated').one(transitionEvent,function(){
-        $outNode.removeClass('pt-page-current');
-        $inNode.removeClass(inClass+' animated');
-        self.isAnimating = false;
-      })
-    }
-  };
-
-  SlidePage.prototype.prePage = function() {
-    if(this.animcursorCheck('pre') && this.index != 0){
-      var outClass = this.animationArray[this.index],
-          $outNode = this.pageArray.eq(this.index),
-          inClass = this.animationArray[--this.index],
-          $inNode = this.pageArray.eq(this.index),
-          self = this;
-
-      this.isAnimating = true;
-      window.a = inClass;
-      window.$a = $inNode;
-      window.b = outClass;
-      window.$b = $outNode;
-      $inNode.addClass('pt-page-current');
+    		this.isAnimating = true;
     
-      //$outNode.addClass(outClass+' animated');
-      /*$inNode.addClass(inClass+'pt-page-current').one(transitionEvent,function(){
-        $outNode.removeClass(outClass+' animated pt-page-current');
-        $inNode.removeClass(inClass+' animated');
-        self.isAnimating = false;
-      })*/
-      $outNode.addClass(outClass+' out animated').one(transitionEvent,function(){
-        $outNode.removeClass(outClass+' out animated pt-page-current');
-        self.isAnimating = false;
-      })
+    		$inNode.addClass(inClass+' pt-page-current').one(transitionEvent,function(){
+          self._showPage(inIndex,outIndex);
+       		$outNode.removeClass('pt-page-current');
+        	$inNode.removeClass(inClass);
+        	self.isAnimating = false;
+      	})
+    	}
+  	};
+
+  	SlidePage.prototype.prePage = function() {
+    	if(!this.isAnimating  && this.index != 0){
+
+    		var outIndex = this.index,
+            inIndex = --this.index,
+            outClass = this.animationArray[outIndex]+' out animated',
+            $outNode = this.pageArray.eq(outIndex),
+            inClass = this.animationArray[inIndex],
+            $inNode = this.pageArray.eq(inIndex),
+        		self = this;
+        
+        this.isAnimating = true;
+        $inNode.addClass('pt-page-current');
+
+    		$outNode.addClass(outClass).one(transitionEvent,function(){
+      		$outNode.removeClass(outClass+' pt-page-current');
+          self._showPage(inIndex,outIndex);
+      		self.isAnimating = false;
+    		})
+    	}
+  	};
+
+    SlidePage.prototype._showPage = function(inIndex,outIndex){
+      this.childrenNodes[inIndex].removeClass("removeable");
+      if(outIndex){
+        this.childrenNodes[outIndex].addClass("removeable");
+      }
+      this._lazyLoad(inIndex);
+      /*for(var i = 0, len = eleArray.length; i < len; i++){
+        var ele = eleArray.eq(i),
+            delaySecond = ele.data('delay');
+        if(!!delaySecond){
+          ele.css({
+            'transition-delay' : delaySecond+'s',
+            '-moz-transition-delay' : delaySecond+'s',
+            '-webkit-transition-delay' : delaySecond+'s',
+            '-o-transition-delay' : delaySecond+'s'
+          }); 
+        }
+      }*/
+
+      /*show node*/
+      //eleArray.removeClass("removeable");
+    };
+
+    SlidePage.prototype._lazyLoad = function(index){
+      for(var i = index, len = index+this.preLoadNum; i<len; i++){
+        var $node = this.pageArray.eq(i);
+        if(this.lazyArray[i]===0){
+          /*子节点的处理*/
+          var lazyItems = $node.find("."+this.lazyClass);
+          /*父节点的处理*/
+          $node.hasClass(this.lazyClass) ? lazyItems.push($node) : "";
+
+          $.each(lazyItems,function(index,item){
+            var $item = $(item),
+                src = $item.attr("data-src"),
+                bg = $item.attr("data-bg");
+            
+            bg ? $item.css("background-image", "url("+bg+")") : "";
+            src ? $item.attr('src',src) : "";
+          });
+
+          this.lazyArray[i] = 1;
+        }
+      }
+     
+    };
+
+  	if ( typeof module != 'undefined' && module.exports ) {
+    	module.exports = SlidePage;
+    } else {
+    	window.SlidePage = SlidePage;
     }
-  };
-
- 
-
-  if ( typeof module != 'undefined' && module.exports ) {
-    module.exports = SlidePage;
-  } else {
-    window.SlidePage = SlidePage;
-  }
-})(window,$)
+})(window,Zepto)
